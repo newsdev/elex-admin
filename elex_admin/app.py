38d8fc6dbe884c74e79669c33ecb343b6ab3c4b8
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-01-07 15:46:26 -0500

    Preparing for deployment.

--- elex_admin/app.py
+++ elex_admin/app.py
@@ -13,47 +13,16 @@ import utils
 
 app = Flask(__name__)
 
-def update_views():
-    models.database.execute_sql(utils.ELEX_RESULTS_VIEW_COMMAND)
-    models.database.execute_sql(utils.ELEX_RACE_VIEW_COMMAND)
-    models.database.execute_sql(utils.ELEX_CANDIDATE_VIEW_COMMAND)
-
-def clean_payload(payload):
-    for k,v in payload.items():
-        v = v[0]
-        if v == u'':
-            v = None
-        if v == 'true':
-            v = True
-        if v == 'false':
-            v = False
-        payload[k] = v
-    return payload
-
-def update_model(cls, payload):
-    for k,v in payload.items():
-        setattr(cls,k,v)
-    cls.save()
-
-def set_winner(candidateid, raceid):
-    print candidateid, raceid
-
-def build_context():
-    context = {}
-    context['CDN_URL'] = utils.CDN_URL
-    context['RACEDATE'] = utils.RACEDATE
-    return dict(context)
-
-@app.route('/elex/')
+@app.route('/elections/2016/admin/')
 def race_list():
-    context = build_context()
+    context = utils.build_context()
     context['races'] = models.ElexRace.select()
     return render_template('race_list.html', **context)
 
-@app.route('/elex/race/<raceid>/', methods=['GET', 'POST'])
+@app.route('/elections/2016/admin/race/<raceid>/', methods=['GET', 'POST'])
 def race_detail(raceid):
     if request.method == 'GET':
-        context = build_context()
+        context = utils.build_context()
         context['race'] = models.ElexRace.get(models.ElexRace.raceid == raceid)
         context['candidates'] = models.ElexCandidate.select().where(models.ElexCandidate.nyt_races.contains(int(raceid)))
         return render_template('race_detail.html', **context)
@@ -66,25 +35,25 @@ def race_detail(raceid):
             r = models.OverrideRace.create(race_raceid=raceid)
 
         if payload.get('nyt_winner', None):
-            set_winner(payload['nyt_winner'], raceid)
+            utils.set_winner(payload['nyt_winner'], raceid)
 
-        update_model(r, payload)
-        update_views()
+        utils.update_model(r, payload)
+        utils.update_views(models.database)
 
         return json.dumps({"message": "success"})
 
-@app.route('/elex/candidate/<candidateid>/', methods=['POST'])
+@app.route('/elections/2016/admin/candidate/<candidateid>/', methods=['POST'])
 def candidate_detail(candidateid):
     if request.method == 'POST':
-        payload = clean_payload(dict(request.form))
+        payload = utils.clean_payload(dict(request.form))
 
         try:
             oc = models.OverrideCandidate.get(models.OverrideCandidate.candidate_candidateid == candidateid)
         except models.OverrideCandidate.DoesNotExist:
             oc = models.OverrideCandidate.create(candidate_candidateid=candidateid)
 
-        update_model(oc, payload)
-        update_views()
+        utils.update_model(oc, payload)
+        utils.update_views(models.database)
 
         return json.dumps({"message": "success"})
 
--- elex_admin/utils.py
+++ elex_admin/utils.py
@@ -1,7 +1,6 @@
 import os
 
-RACEDATE = os.environ.get('RACEDATE', None)
-CDN_URL = os.environ.get('ELEX_ADMIN_CDN_URL', None)
+CDN_URL = os.environ.get('ELEX_ADMIN_CDN_URL', 'http://int.nyt.com.s3.amazonaws.com/cdn')
 
 ELEX_RACE_VIEW_COMMAND = """CREATE OR REPLACE VIEW elex_races as
    SELECT o.*, r.* from races as r
@@ -18,3 +17,53 @@ ELEX_RESULTS_VIEW_COMMAND = """CREATE OR REPLACE VIEW elex_results as
        LEFT JOIN override_candidates as c on r.candidateid = c.candidate_candidateid
        LEFT JOIN override_races as o on r.raceid = o.race_raceid
 ;"""
+
+RACEDATE = os.environ.get('RACEDATE', None)
+
+def update_views(database):
+    """
+    Resets the Postgres VIEWs.
+    """
+    database.execute_sql(utils.ELEX_RESULTS_VIEW_COMMAND)
+    database.execute_sql(utils.ELEX_RACE_VIEW_COMMAND)
+    database.execute_sql(utils.ELEX_CANDIDATE_VIEW_COMMAND)
+
+def clean_payload(payload):
+    """
+    Serializes payload from form strings to more useful Python things.
+    """
+    for k,v in payload.items():
+        v = v[0]
+        if v == u'':
+            v = None
+        if v == 'true':
+            v = True
+        if v == 'false':
+            v = False
+        payload[k] = v
+    return payload
+
+def update_model(cls, payload):
+    """
+    Given a model class and a set of keys/values, will
+    update and save that model class with the keys/values.
+    """
+    for k,v in payload.items():
+        setattr(cls,k,v)
+    cls.save()
+
+def set_winner(candidateid, raceid):
+    """
+    This is the part where I might need to do more things?
+    Will check in with Wilson.
+    """
+    print candidateid, raceid
+
+def build_context():
+    """
+    Every page needs these two things.
+    """
+    context = {}
+    context['CDN_URL'] = CDN_URL
+    context['RACEDATE'] = RACEDATE
+    return dict(context)
