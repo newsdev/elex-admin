1adb2dae446e4042ffa6ebfa5e16a4b173cfc97b
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-03-12 09:22:26 -0500

    Fix delayed db loading

--- elex_admin/initialize_racedate.py
+++ elex_admin/initialize_racedate.py
@@ -1,5 +1,10 @@
 import models
 
 if __name__ == "__main__":
+    racedate_db = PostgresqlExtDatabase('elex_%s' % os.environ.get('RACEDATE', None),
+        user=os.environ.get('ELEX_ADMIN_USER', 'elex'),
+        host=os.environ.get('ELEX_ADMIN_HOST', '127.0.0.1')
+    )
+    models.database_proxy.initialize(racedate_db)
     models.OverrideCandidate.add_candidates()
     models.OverrideRace.create_override_races()
