99ccf63b85041b6b6d07a127235b486b5f5b0b8f
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-11-03 11:47:21 -0400

    use http basic auth

--- elex_admin/create_configs.py
+++ elex_admin/create_configs.py
@@ -28,16 +28,8 @@ if __name__ == "__main__":
             with open('elex_admin/%s.ini' % r, 'w') as writefile:
                 writefile.write(text)
         nginx_conf += """    location ^~ /elections/2016/admin/%s/ {
-        allow 10.0.0.0/8; 
-        allow 170.149.100.0/24;
-        allow 78.25.224.224/27;
-        allow 202.147.18.187/32;
-        allow 206.205.234.166/32;
-        allow 50.232.13.178/32;
-        allow 170.149.100.70/32;
-        allow 23.21.126.6/32;
-        allow 107.20.172.193/32;
-        deny all;
+        auth_basic "Restricted Content";
+        auth_basic_user_file /etc/nginx/.htpasswd;
         proxy_pass http://127.0.0.1:%s;
     }
 """ % (r, port)
