2f21ebc9532bb0d7a951fcfef1380561ad0b7df6
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-12-10 17:06:52 -0500

    Move new models to the old models

--- elex_admin/models.py
+++ elex_admin/models.py
@@ -4,8 +4,8 @@ import os
 from peewee import *
 from playhouse.postgres_ext import *
 
-import utils
-
+import general.utils as utils
+from maps import PREZ_REPORTING_UNIT_TO_DISTRICT_LABELS, PREZ_STATE_TO_LABEL
 
 database_proxy = Proxy()
 
@@ -15,232 +15,84 @@ class BaseModel(Model):
 
 
 class OverrideCandidate(BaseModel):
-    nyt_candidate_description = TextField(null=True)
-    nyt_candidate_name = CharField(null=True)
-    nyt_races = ArrayField(field_class=CharField)
-    nyt_candidate_important = BooleanField(null=True)
+    candidate_unique_id = CharField(db_column='candidate_unique_id', null=True, primary_key=True)
+    nyt_name = CharField(null=True)
     nyt_winner = BooleanField(null=True)
-    nyt_display_order = IntegerField(null=True)
-    nyt_delegates = IntegerField(null=True)
-    id = CharField(primary_key=True)
+    nyt_electwon = IntegerField(null=True)
+
+    reportingunitid = CharField(null=True)
+    raceid = CharField(null=True)
+    statepostal = CharField(null=True)
 
     class Meta:
         db_table = 'override_candidates'
 
-    def serialize(self):
-        return collections.OrderedDict([
-            utils.make_field(self, 'nyt_candidate_name'),
-            utils.make_field(self, 'nyt_candidate_important'),
-            utils.make_field(self, 'nyt_candidate_description'),
-            ('nyt_races', "{%s}" % ",".join([unicode(r) for r in self.nyt_races])),
-            utils.make_field(self, 'nyt_display_order'),
-            utils.make_field(self, 'nyt_winner'),
-            utils.make_field(self, 'nyt_delegates'),
-            utils.make_field(self, 'id')
-        ])
-
-    @classmethod
-    def add_prez_candidates(cls):
-        races = ElexRace.select().where(ElexRace.raceid == "0")
-        for race in list(races):
-            print "President: %s" % race.statepostal
-
-            candidates = ElexResult.select()\
-                            .where(ElexResult.statepostal == race.statepostal, ElexResult.raceid == race.raceid)\
-                            .where(ElexResult.level << ['state', 'national'])
-
-            candidates = sorted([e for e in candidates], key=lambda x: x.last)
-
-            for idx, candidate in enumerate(candidates):
-                try:
-                    oc = cls.get(cls.id == candidate.candidate_unique_id)
-                except cls.DoesNotExist:
-                    oc = cls.create(id=candidate.candidate_unique_id)
-
-                if not oc.nyt_races:
-                    oc.nyt_races = []
-
-                oc.nyt_races.append("%s-%s" % (race.statepostal,race.raceid))
-                oc.nyt_display_order = idx
-                oc.save()
-
-    @classmethod
-    def add_candidates(cls):
-        races = ElexRace.select().where(ElexRace.raceid != "0")
-
-        for race in list(races):
-            print race.statepostal, race.officename, race.seatname
-
-            candidates = list(race.state())
-            for idx, candidate in enumerate(candidates):
-
-                try:
-                    oc = cls.get(cls.id == candidate.candidate_unique_id)
-                except cls.DoesNotExist:
-                    oc = cls.create(id=candidate.candidate_unique_id)
-
-                oc.nyt_races = ["%s-%s" % (race.statepostal,race.raceid)]
-                oc.nyt_display_order = idx
-                oc.save()
-
-        database_proxy.execute_sql(utils.ELEX_RESULTS_VIEW_COMMAND)
-        database_proxy.execute_sql(utils.ELEX_CANDIDATE_VIEW_COMMAND)
-
 
 class OverrideRace(BaseModel):
-    nyt_race_preview = TextField(null=True)
-    nyt_race_result_description = TextField(null=True)
-    nyt_delegate_allocation = TextField(null=True)
-    report = BooleanField(null=True)
-    report_description = TextField(null=True)
     accept_ap_calls = BooleanField(null=True)
-    nyt_race_description = TextField(null=True)
-    nyt_race_name = CharField(null=True)
-    race_raceid = CharField(primary_key=True)
-    race_statepostal = CharField(null=True)
     nyt_called = BooleanField(null=True)
-    nyt_race_important = BooleanField(null=True)
+    nyt_race_description = CharField(null=True)
+    nyt_race_name = CharField(null=True)
+    race_unique_id = TextField(db_column='race_unique_id', null=True, primary_key=True)
+    report = BooleanField(null=True)
+
+    reportingunitid = CharField(null=True)
+    raceid = CharField(null=True)
+    statepostal = CharField(null=True)
 
     class Meta:
         db_table = 'override_races'
 
-    def serialize(self):
-        return collections.OrderedDict([
-            utils.make_field(self, 'nyt_race_preview'),
-            utils.make_field(self, 'nyt_race_result_description'),
-            utils.make_field(self, 'nyt_delegate_allocation'),
-            utils.make_field(self, 'report'),
-            utils.make_field(self, 'report_description'),
-            utils.make_field(self, 'race_raceid'),
-            utils.make_field(self, 'race_statepostal'),
-            utils.make_field(self, 'nyt_race_name'),
-            utils.make_field(self, 'nyt_race_description'),
-            utils.make_field(self, 'accept_ap_calls'),
-            utils.make_field(self, 'nyt_called'),
-            utils.make_field(self, 'nyt_race_important')
-        ])
-
-    @classmethod
-    def create_override_races(cls):
-        races = ElexRace.select()
-        for race in list(races):
-            try:
-                r = cls.get(cls.race_raceid == race.raceid, cls.race_statepostal == race.statepostal)
-            except cls.DoesNotExist:
-                r = cls.create(race_raceid=race.raceid, race_statepostal=race.statepostal)
-
-        database_proxy.execute_sql(utils.ELEX_RESULTS_VIEW_COMMAND)
-        database_proxy.execute_sql(utils.ELEX_CANDIDATE_VIEW_COMMAND)
-
-
-class ElexCandidate(BaseModel):
-    nyt_candidate_description = TextField(null=True)
-    nyt_candidate_name = CharField(null=True)
-    nyt_races = ArrayField(field_class=CharField)
-    ballotorder = IntegerField(null=True)
-    first = CharField(null=True)
-    id = CharField(null=True)
-    last = CharField(null=True)
-    party = CharField(null=True)
-    polid = CharField(null=True)
-    polnum = CharField(null=True)
-    nyt_candidate_important = BooleanField(null=True)
-    nyt_winner = BooleanField(null=True)
-    nyt_display_order = IntegerField(null=True)
-    nyt_delegates = IntegerField(null=True)
-
-    class Meta:
-        db_table = 'elex_candidates'
-
-    def __unicode__(self):
-        if self.nyt_candidate_name:
-            return self.nyt_candidate_name
-        return "%s %s" % (self.first, self.last)
-
+    @property
+    def state_label(self):
+        if "district" in self.reportingunitid:
+            return PREZ_REPORTING_UNIT_TO_DISTRICT_LABELS[self.reportingunitid]['label']
+        # use full state names
+        return PREZ_STATE_TO_LABEL[self.statepostal]
+        # return self.statepostal
 
 class ElexRace(BaseModel):
-    nyt_race_preview = TextField(null=True)
-    nyt_race_result_description = TextField(null=True)
-    nyt_delegate_allocation = TextField(null=True)
-    report = BooleanField(null=True)
-    report_description = TextField(null=True)
-    accept_ap_calls = BooleanField(null=True)
-    nyt_race_description = TextField(null=True)
-    nyt_race_name = CharField(null=True)
-    race_raceid = CharField(primary_key=True)
-    race_statepostal = CharField(null=True)
-    description = CharField(null=True)
-    id = CharField(null=True)
-    initialization_data = BooleanField(null=True)
-    lastupdated = DateField(null=True)
-    national = BooleanField(null=True)
-    officeid = CharField(null=True)
-    officename = CharField(null=True)
-    party = CharField(null=True)
     raceid = CharField(null=True)
-    racetype = CharField(null=True)
-    racetypeid = CharField(null=True)
-    seatname = CharField(null=True)
-    seatnum = CharField(null=True)
-    statename = CharField(null=True)
+    reportingunitid = CharField(null=True)
     statepostal = CharField(null=True)
-    test = BooleanField(null=True)
-    uncontested = BooleanField(null=True)
-    nyt_race_important = BooleanField(null=True)
-    nyt_called = BooleanField(null=True)
+
+    accept_ap_calls = BooleanField(null=True)
+    officeid = CharField(null=True)
+    national = BooleanField(null=True)
+    race_unique_id = TextField(db_column='race_unique_id', null=True, primary_key=True)
 
     class Meta:
         db_table = 'elex_races'
 
-    def __unicode__(self):
-        return "%s %s %s %s" % (self.statepostal, self.party, self.officename, self.racetype)
-
-    def state(self):
-        results = ElexResult.select()\
-                            .where(ElexResult.statepostal == self.statepostal, ElexResult.raceid == self.raceid)\
-                            .where(ElexResult.level == 'state')
-        if len(results) == 0:
-            results = ElexResult.select()\
-                            .where(ElexResult.statepostal == self.statepostal, ElexResult.raceid == self.raceid)\
-                            .where(ElexResult.level == None)
-        return sorted([e for e in results], key=lambda x: x.last)
-
-    def counties(self):
-        results = ElexResult.select()\
-                            .where(ElexResult.statepostal == self.statepostal, ElexResult.raceid == self.raceid)\
-                            .where(ElexResult.level == 'county')
-        return sorted([e for e in results], key=lambda x: x.last)
-
-    def townships(self):
-        results = ElexResult.select()\
-                            .where(ElexResult.statepostal == self.statepostal, ElexResult.raceid == self.raceid)\
-                            .where(ElexResult.level == 'county')
-        return sorted([e for e in results], key=lambda x: x.last)
-
-    def candidates(self):
-        return None
+    @property
+    def state_label(self):
+        # use full state names
+        return PREZ_STATE_TO_LABEL[self.statepostal]
 
 
 class ElexResult(BaseModel):
-    nyt_race_preview = TextField(null=True)
-    nyt_race_result_description = TextField(null=True)
-    nyt_delegate_allocation = TextField(null=True)
-    report = BooleanField(null=True)
-    report_description = TextField(null=True)
-    nyt_candidate_description = TextField(null=True)
-    nyt_candidate_name = CharField(null=True)
-    accept_ap_calls = BooleanField(null=True)
-    nyt_race_description = TextField(null=True)
-    nyt_race_name = CharField(null=True)
+    nyt_name = CharField(null=True)
     nyt_winner = BooleanField(null=True)
-    race_raceid = CharField(null=True)
-    race_statepostal = CharField(null=True)
+    nyt_electwon = IntegerField(null=True)
+
+    accept_ap_calls = BooleanField(null=True)
+    nyt_called = BooleanField(null=True)
+    nyt_race_description = CharField(null=True)
+    nyt_race_name = CharField(null=True)
+    race_unique_id = TextField(db_column='race_unique_id', null=True)
+    report = BooleanField(null=True)
+
     ballotorder = IntegerField(null=True)
+    candidate_unique_id = CharField(db_column='candidate_unique_id', null=True)
     candidateid = CharField(null=True)
+    delegatecount = IntegerField(null=True)
     description = CharField(null=True)
+    electiondate = CharField(null=True)
+    electtotal = IntegerField(null=True)
+    electwon = IntegerField(null=True)
     fipscode = CharField(null=True)
     first = CharField(null=True)
-    id = CharField(null=True)
+    id = CharField(null=True, primary_key=True)
     incumbent = BooleanField(null=True)
     initialization_data = BooleanField(null=True)
     is_ballot_position = BooleanField(null=True)
@@ -271,13 +123,6 @@ class ElexResult(BaseModel):
     votecount = IntegerField(null=True)
     votepct = DecimalField(null=True)
     winner = BooleanField(null=True)
-    nyt_race_important = BooleanField(null=True)
-    nyt_candidate_important = BooleanField(null=True)
-    nyt_called = BooleanField(null=True)
-    nyt_display_order = IntegerField(null=True)
-    nyt_delegates = IntegerField(null=True)
-    candidate_unique_id = CharField(null=True)
 
     class Meta:
-        db_table = "elex_results"
-
+        db_table = 'elex_results'
